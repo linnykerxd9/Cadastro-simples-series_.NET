c5f09f05f8d9bcbb7406818e983bbee98b7af505
linnykerxd9 <linnykerxd9@gmail.com>
2021-06-18 19:05:31 -0300

    Finalizando o projeto

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -10,6 +10,7 @@ namespace Dio.series
         private string Descricao { get; set; }
         private int Ano { get; set; }
 
+        private bool Excluido { get; set; }
         public Serie(int id, Genero genero, string titulo, string descricao, int ano)
         {
 
@@ -18,16 +19,17 @@ namespace Dio.series
             this.Genero = genero;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
         {
-
             string retorno = "";
-            retorno += "Gênero" + this.Genero + Environment.NewLine;
-            retorno += "Titulo" + this.Titulo + Environment.NewLine;
-            retorno += "Descrição" + this.Descricao + Environment.NewLine;
-            retorno += "Ano de Inicio" + this.Ano;
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Inicio: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido:" + this.Excluido;
             return retorno;
         }
 
@@ -38,6 +40,13 @@ namespace Dio.series
         public int retornaId()
         {
             return this.Id;
+        }
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir(){
+            this.Excluido = true;
         }
     }
 }
