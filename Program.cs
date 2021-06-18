5f8da5b12808e4aba3eaf2a378599af9c0442f3a
linnykerxd9 <linnykerxd9@gmail.com>
2021-06-18 17:38:17 -0300

    Criando a classe base e a clase de series

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace Dio.series
     {
         static void Main(string[] args)
         {
+            Serie minhaSerie = new Serie();
+                
             Console.WriteLine("Hello World!");
         }
     }
